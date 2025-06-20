66f72289645bd44e31b0ac86b3922c420fdd8df2
Malo Legendre-Lemaire <malo.legendre-lemaire@epita.fr>
2025-06-20 12:17:26 +0200

    feat: Test download wasm-opt in build-wasm

--- build-wasm.sh
+++ build-wasm.sh
@@ -6,8 +6,15 @@ mkdir -p dist
 rm -rf dist/*
 cargo build -p visualisation --release --target wasm32-unknown-unknown
 cp -r public/* dist
+
 if [ -x "$(command -v wasm-opt)" ]; then
-  wasm-opt -O3 --strip-debug target/wasm32-unknown-unknown/release/visualisation.wasm -o dist/visualisation.wasm
+  WASMOPT=wasm-opt
 else
-  cp target/wasm32-unknown-unknown/release/visualisation.wasm dist/visualisation.wasm
+  dir=$(mktemp -d)
+  wget https://github.com/WebAssembly/binaryen/releases/download/version_123/binaryen-version_123-x86_64-linux.tar.gz -O "$dir/binaryen.tar.gz"
+  ( cd $dir; tar -xvf ./binaryen.tar.gz; )
+  WASMOPT="$dir/binaryen-version_123/bin/wasm-opt"
+  # cp target/wasm32-unknown-unknown/release/visualisation.wasm dist/visualisation.wasm
 fi
+
+$WASMOPT -O3 --strip-debug target/wasm32-unknown-unknown/release/visualisation.wasm -o dist/visualisation.wasm
